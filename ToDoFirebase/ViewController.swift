7a98113dd20045a5417106bf0f759af23f5fc0f1
blade8323 <blade8323@mail.ru>
2022-04-14 21:43:27 +0500

    Initial Commit

---
+++ ToDoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFirebase
+//
+//  Created by Владислав Соколов on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
